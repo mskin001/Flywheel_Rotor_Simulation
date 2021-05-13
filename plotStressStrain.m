c63fbcc4e24e5d52f8353c9a003e4c90a7d863b7
Miles Skinner <skinner.miles@gmail.com>
2021-05-13 10:17:47 -0600

    update

--- plotStressStrain.m
+++ plotStressStrain.m
@@ -22,18 +22,16 @@ or = rim;
 halfWay = round(length(sArr)/2);
 
 if strcmp(plotWhat.custom1, 'yes')
-  rad_data = csvread('radial stress ansys.csv',...
-    1,1,[1,1,17,4]);
-  hoop_data = csvread('hoop stress ansys.csv',...
-    1,1,[1,1,17,4]);
+  rad_data = csvread('Apa2011_10xradial.csv');
+  hoop_data = csvread('Apa2011_hoop.csv');
   hold on
   plot(rArr*1000, (sArr{1}(3,:,1)/mat.stren{1}(3)),'-', 'Color', [0 0.4470 0.7410], 'Linewidth', 1.5)
   plot(rArr*1000, (sArr{1}(1,:,1)/mat.stren{1}(1)), '--', 'Color', [0.6350 0.0780 0.1840], 'Linewidth', 1.5)
   plot(rad_data(:,1)*1000,(rad_data(:,4)/mat.stren{1}(3)), 'kv')
   plot(hoop_data(:,1)*1000,(hoop_data(:,4)/mat.stren{1}(1)), 'k^')
-%   plot(rArr*1000, tau{1}*10^-6, ':', 'Color', [0.4940 0.1840 0.5560], 'Linewidth', 1.5)
-%   stressData = csvread('aparicio2011_results.csv', 1, 0);
-%   plot(stressData(:,1)*1000, stressData(:,2), 'ko')
+  plot(rArr*1000, tau{1}*10^-6, ':', 'Color', [0.4940 0.1840 0.5560], 'Linewidth', 1.5)
+  stressData = csvread('aparicio2011_results.csv', 1, 0);
+  plot(stressData(:,1)*1000, stressData(:,2), 'ko')
 
   xlabel('Radius [mm]')
   ylabel('Stress [MPa]')
