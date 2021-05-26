3d1d0d75e3400a4967017523ae78c2c083c05244
Miles Skinner <skinner.miles@gmail.com>
2021-05-25 23:57:22 -0600

    update

--- main.m
+++ main.m
@@ -21,24 +21,25 @@ st = 'pe';
 % Rotor
 % rim = [0.03789; 0.07901]; % single rim Ha 1999
 % rim = [.1, 0.8];
-% rim = [.05, .1];
-rim = [0.08, 0.2]; % Perez-Aparicio 2011
+rim = [.12, .24];
+h = 0.05; % [m]
+% rim = [0.08, 0.2]; % Perez-Aparicio 2011
 % rim = [0.0762, .1524]; % Tzeng2001
 rdiv = 30; % number of points per rim to analyze
-delta = [0]/1000; % [mm]
+delta = [0]; % [m]
 sigb = [0, 0];
 % mats = {'salehian_Incl718.mat'};
-mats = {'GFRP_Aparicio2011.mat'};
+mats = {'IM7_8552_Tzeng2001.mat'};
 
 % Time/creep
-tmax = 0.005; %seconds?
-tStep = 0.005; %second between steps
+tmax = 1; %seconds?
+tStep = 1; %second between steps
 simTime = tmax;
 timeUnit = 's'; % s = sec, h = hours, d = days
 compFunc = {'no'}; % compliance function, input 'no' to turn off creep modeling
 
 % Speed/velocity
-rpm = 17.452;
+rpm = 30000;
 % rpmMax = 97682;
 accType = 'const';
 
@@ -47,7 +48,7 @@ accType = 'const';
 % main.m and shearStress.m. Apply changes with caution.
 if strcmp(accType, 'const')
   % Constant
-  alpha = @(t,wIni) 3.6e5 * t; % gets multiplied by tStep at end of while loop
+  alpha = @(t,wIni) 0 * t; % gets multiplied by tStep at end of while loop
   % alpha = @(t,wIni) 3.6e6 * t;
 elseif strcmp(accType, 'Linear')
   % Linear Acceleration:
@@ -67,13 +68,13 @@ end
 % Plotting
 % legTxt = {'Current model', 'Aparicio 2011'};
 legTxt = {'0 sec', '5 sec', '10 sec', '15 sec', '20 sec'}; % Controls legend entries for graphs
-plotWhat.custom1 = 'yes';        % any custom plot. Go to plotStressStrain.m to modify (first if statement)
+plotWhat.custom1 = 'no';        % any custom plot. Go to plotStressStrain.m to modify (first if statement)
 plotWhat.radDis = 'no';          % Radial displacement v. radius
 plotWhat.radStr = 'yes';         % Radial stress v. radius plot
 plotWhat.hoopStr = 'yes';        % Hoop stress v. radius plot
-plotWhat.shearStr = 'yes';       % Shear stress v. radius
+plotWhat.shearStr = 'no';       % Shear stress v. radius
 plotWhat.peakStr = 'no';        % 2-yaxis plot. Peak stress location and SR v. time
-plotWhat.sr = 'no';
+plotWhat.sr = 'yes';
 
 plotWhat.disGif = 'no';          % Displacement gif, surface plot
 plotWhat.disGifName = 'Displacement.gif';
@@ -160,7 +161,6 @@ fprintf('Check Input Variables: Complete\n')
 %% -----------------------------------------------------------------------------
 % Program Begin
 % ------------------------------------------------------------------------------
-% vel = zeros(1,tmax/tStep);
 w = (pi/30) * rpm; %initial angular velocity
 w0 = w;
 vari = cast(tmax/tStep,'single');
@@ -248,6 +248,9 @@ while b*tStep <= tmax
 
 end
 
+[E, P] = find_energy_power(h, tStep, alpha, accType); % Calculate energy and power
+                                                      % assuming constant
+                                                      % acceleration
 %% -----------------------------------------------------------------------------
 % Calculate failure criterion
 % ------------------------------------------------------------------------------
